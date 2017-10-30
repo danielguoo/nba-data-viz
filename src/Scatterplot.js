831db366a847ed48067feb1f2be8f3d63582872c
Daniel Guo <danielg216@gmail.com>
2017-10-29 20:38:35 -0700

    Readme

---
+++ src/Scatterplot.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {XYPlot, XAxis, YAxis, MarkSeries, Hint, HorizontalGridLines, VerticalGridLines} from 'react-vis';
+
+export default class Scatterplot extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {value: null}
+		this.rememberValue = this.rememberValue.bind(this)
+		this.forgetValue = this.forgetValue.bind(this)
+	}
+
+	rememberValue(value) {
+		this.setState({value})
+	}
+
+	forgetValue() {
+		this.setState({value: null})
+	}
+
+	render(){
+		const {data} = this.props
+		return (
+			<XYPlot
+        width={800}
+        height={500}>
+        <HorizontalGridLines/>
+        <VerticalGridLines/>
+        <XAxis />
+        <YAxis />
+        <MarkSeries
+        	colorType="literal"
+          data={data}
+          onValueMouseOver={this.rememberValue}
+        	onValueMouseOut={this.forgetValue}/>
+        {this.state.value ? 
+     		<Hint value={this.state.value}>
+  				<div style={{background: 'black'}}>
+    				<h3>{this.state.value.playerName}</h3>
+  				</div>
+				</Hint> : null }
+     	</XYPlot>
+		);
+	}
+}
+
